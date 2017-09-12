9c44cd9034e35e2b1d8320c2147b7ce90272b07a
Alexey Stepanov <penguinolog@gmail.com>
2017-09-12 13:16:27 +0300

    cleanup and chmod after build [ci skip]

--- tools/build-wheels.sh
+++ tools/build-wheels.sh
@@ -46,3 +46,8 @@ for PYTHON in ${PYTHON_VERSIONS}; do
     /opt/python/${PYTHON}/bin/pip install pytest
     /opt/python/${PYTHON}/bin/py.test -vv /io/test
 done
+
+find /io/dist/ -type f -not -name "*$package_name*" -exec rm {} +
+
+chmod -v a+rwx /io/dist
+chmod -v a+rw /io/dist/*
