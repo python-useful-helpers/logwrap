a0e8d3f6f1e4d26da0010f67993b8ebeba8d83a4
Alexey Stepanov <penguinolog@users.noreply.github.com>
2018-11-12 10:56:47 +0100

    Bump 5.2.1

--- logwrap/__init__.py
+++ logwrap/__init__.py
@@ -37,7 +37,7 @@ __all__ = (
     "bind_args_kwargs",
 )
 
-__version__ = "5.2.0"
+__version__ = "5.2.1"
 __author__ = "Alexey Stepanov"
 __author_email__ = "penguinolog@gmail.com"
 __maintainers__ = {
