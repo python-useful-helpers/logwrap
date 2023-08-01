92af51064d2a0a2aeea949ebcbe69bba5620929b
Aleksei Stepanov <alekseis@nvidia.com>
2023-08-01 08:07:36 +0200

    Updated ruff: less false-negative

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ def get_simple_vars_from_src(
             continue
         try:
             value = ast.literal_eval(node.value)
-        except ValueError:  # noqa: PERF203
+        except ValueError:
             continue
         for tgt in node.targets:
             if isinstance(tgt, ast.Name) and isinstance(tgt.ctx, ast.Store):
